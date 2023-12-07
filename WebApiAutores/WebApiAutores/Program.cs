b421b71aa72f79c573ae999affd9fc8cbd618c59
Alan Dono <alannicolasdono@gmail.com>
2023-12-07 03:40:05 -0300

    Clase Starup terminada

--- WebApiAutores/WebApiAutores/Program.cs
+++ WebApiAutores/WebApiAutores/Program.cs
@@ -1,25 +1,15 @@
+using WebApiAutores;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var startup = new Startup(builder.Configuration);
+startup.ConfigurationServices(builder.Services);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+startup.Configure(app, app.Environment);
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
